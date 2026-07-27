f3a51a952734fc314b63743f8b54a0baa9e8e2d5
fetehadin <fetehadinnegash@gmail.com>
2026-07-27 13:39:10 +0300

    Remove allowedDevOrigins for production build

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
-    allowedDevOrigins: ['192.168.43.89'],
-  }
 };
 
 export default nextConfig;
